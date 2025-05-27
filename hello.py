28ecdd0b739cd60f7751fc17dc216a241ca570f0
Tannerzhu1 <108439277+Tannerzhu1@users.noreply.github.com>
2025-05-27 11:57:13 -0700

    Added name function

--- hello.py
+++ hello.py
@@ -4,11 +4,19 @@ def main():
     
     
 def greet():
+    p1_age = age()
+    p1_name = name()
+    print("\033[2J", end="")
     print(f"Hello world!")
-    print(f"I'm {age()} years old")
+    print(f"I'm {p1_name} and I'm {p1_age} years old")
 
 def age():
     age = int(input("Enter your age: "))
     return age
+
+def name():
+    name = input("Enter your name: ")
+    return name
+
 
 main()
