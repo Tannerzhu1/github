06d87210bd76c522a83efd3e9f51add1a79f6a55
Tannerzhu1 <108439277+Tannerzhu1@users.noreply.github.com>
2025-05-27 12:09:00 -0700

    Created a new file to manage functions

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
+import job
+
 def main():
     greet()
 
     
-    
 def greet():
     p1_age = age()
     p1_name = name()
+    p1_employer, p1_title, p1_rank, p1_annual = job.workplace()
     print("\033[2J", end="")
     print(f"Hello world!")
     print(f"I'm {p1_name} and I'm {p1_age} years old")
+    print(f"My company is {p1_employer} and I'm the {p1_rank} of the {p1_title}. I make {p1_annual}")
 
 def age():
     age = int(input("Enter your age: "))
---
+++ job.py
@@ -0,0 +1,14 @@
+def main():
+    print("lol")
+    
+
+
+def workplace():
+    company = input("Employer: ")
+    job_title = input("Title: ")
+    tier = input("Tier: ")
+    income = int(input("Annual Income: "))
+    return company, job_title, tier, income
+
+
+main()
